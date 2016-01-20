e0c6df080ec229b8f70ee61ee87639c579292ea3
Emily <stuckeyemily@gmail.com>
2016-01-20 14:55:10 -0500

    Added SermonIndexMaker class as entry point

---
+++ SermonIndexMaker.java
@@ -0,0 +1,10 @@
+//SermonIndexMaker
+//the actual deal
+//Emily Stuckey
+//1-19-16
+
+public class SermonIndexMaker{
+  public static void main(String[] args){
+    IndexMaker x = new IndexMaker();
+  }
+}
