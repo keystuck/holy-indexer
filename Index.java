060f626c76692950cc9364c8f1b976ab1687025c
Emily <stuckeyemily@gmail.com>
2016-01-20 14:55:52 -0500

    updated

--- Index.java
+++ Index.java
@@ -5,34 +5,73 @@
 //extends ArrayList to be a list of IndexEntries
 
 import java.util.*;
+import java.util.regex.*;
+import java.io.*;
 
 public class Index extends ArrayList<IndexEntry>{
+  public Index(){
 
-  public IndexEntry findOrAdd(IndexEntry newEnt){
+  }
+
+  public Index(String sermon, String fileName){
+    try {
+      BufferedReader sermReader = new BufferedReader(new FileReader(fileName));
+      int para = 0;
+      String nextL = sermReader.readLine();
+      while (nextL != null){
+        Pattern p = Pattern.compile("^\\d+\\.?$");
+        Matcher m = p.matcher(nextL);
+        //if the only thing on the line is a digit
+        //set the para to that digit and advance
+
+        if (m.matches()){
+          para = Integer.parseInt(nextL);
+        }
+        //else add the phrase on that line to the index
+        else
+        {
+          System.out.println("nextL = " + nextL);
+          nextL = nextL.substring(0, 1).toUpperCase() + nextL.substring(1);
+          System.out.println("now it's " + nextL);
+          IndexEntry indEnt = findOrAdd(nextL);
+          indEnt.add(sermon, para);
+
+        }
+        nextL = sermReader.readLine();
+      }
+    } catch (IOException e){
+      System.out.println("IO exception: " + e.getMessage());
+    }
+  }
+
+  public IndexEntry findOrAdd(String newEnt){
+
+    if (this.size() == 0){
+      add(new IndexEntry(newEnt, null));
+      return this.get(0);
+    }
     int i = 0;
     boolean found = false;
     IndexEntry cur = this.get(i);
     while (i < this.size() && !found){
-
-      if (cur.getWord().compareTo(newEnt.getWord())==0){
+      if (cur.getWord().compareTo(newEnt)==0){
         found = true;
-        cur.add(newEnt.getCits());
       }
-      else if (cur.compareTo(newEnt) < 0){
+      else if (cur.getWord().compareTo(newEnt) < 0){
           i++;
           if (i < this.size()){
-            this.get(i);
+            cur = this.get(i);
           }
       }
       else {
-        this.add(i, newEnt);
-        cur = newEnt;
+        this.add(i, new IndexEntry(newEnt, null));
+        cur = this.get(i);
         found = true;
       }
     }
     if (i == this.size()){
-      this.add(i, newEnt);
-      cur = newEnt;
+      this.add(i, new IndexEntry(newEnt, null));
+      cur = this.get(i);
     }
     return cur;
   }
@@ -42,7 +81,7 @@ public class Index extends ArrayList<IndexEntry>{
       return super.add(newEnt);
     }
     else {
-      findOrAdd(newEnt);
+      findOrAdd(newEnt.getWord());
       return true;
     }
   }
--- IndexMaker.java
+++ IndexMaker.java
@@ -4,15 +4,46 @@
 //Emily Stuckey
 //1-10-16
 import java.util.Scanner;
+import java.io.*;
 
 
 public class IndexMaker {
   public IndexMaker(){
     Scanner input = new Scanner(System.in);
-    System.out.print("Enter 1 to create a new Sermon index, 2 to create a new Scriptural index, 3 to merge indices: ");
+    System.out.print("Enter 1 to create a new Sermon index, 2 to create a new Scriptural index, 3 to merge indices, anything else to exit: ");
     int whatWant = input.nextInt();
     if (whatWant == 1){
-      //creating new index
+      boolean done = false;
+notDone:      while (!done){
+        //creating new index
+        System.out.print("Enter number/title of sermon or EXIT to exit: ");
+        String serm = input.next();
+        if (serm.equalsIgnoreCase("exit")){
+          done = true;
+          break notDone;
+        }
+        System.out.print("Enter name of file (remember extension): ");
+        String fName = input.next();
+        if (fName.equalsIgnoreCase("exit")){
+          done = true;
+          break notDone;
+        }
+        try {
+          //Open the given file
+          Index sermIndex = new Index(serm, fName);
+          //create a new DocumentIndex from the file
+          BufferedWriter sermWriter = new BufferedWriter(new FileWriter(serm + "-index.txt"));
+          //Create an output file for that sermon: e.g. "1-index.txt"
+          //write the DocumentIndex to the output file
+
+          sermWriter.write(sermIndex.toString());
+          sermWriter.flush();
+          sermWriter.close();
+        }
+        catch (IOException e){
+          System.out.println("Error in IndexMaker: " + e.getMessage());
+        }
+    }
     }
     else if (whatWant == 2){
       //creating new scriptural index (automatically?)
