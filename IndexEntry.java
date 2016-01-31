ffa56a721c12d632e841ef5e8912bbd3c5638a45
Emily <stuckeyemily@gmail.com>
2016-01-30 22:02:53 -0500

    Added comments to IndexEntry

--- IndexEntry.java
+++ IndexEntry.java
@@ -1,24 +1,40 @@
-//IndexEntry
-//represents the index entry for a single word/phrase
-//example:    Holy Spirit     1.2, 1.5, 4.7
+import java.util.*;
+
+/** IndexEntry
+ * represents the index entry for a single word/phrase
+ * example:    Holy Spirit     1.2, 1.5, 4.7
+ */
 
 import java.util.*;
 
-public class IndexEntry{
+public class IndexEntry implements Comparable<IndexEntry>{
+  /** Word or phrase to be indexed */
   private String word;
+  /** List of citations */
   private List<SermCit> citations;
 
+/** Constructor given phrase & list
+ * @param wd Phrase to index
+ * @param cits List of citations, comma-separated
+ */
   public IndexEntry(String wd, List<SermCit> cits){
     word = wd;
     citations = cits;
   }
 
+  /** Constructor given word, sermon, paragraph number for first reference
+   * @para wd Phrase to index
+   * @param sermon Sermon where reference occurs
+   * @param para Paragraph within sermon
+   */
   public IndexEntry(String wd, String sermon, int para){
     this(wd, null);
     this.add(sermon, para);
   }
 
-//for reading in an indexentry from a file
+/** Constructor given a string containing phrase & one reference
+ * @param entry String e.g. "Virgin Mary\t1.3"
+ */
   public IndexEntry(String entry){
     citations = new ArrayList<SermCit>();
     word = entry.substring(0, entry.indexOf("\t"));
@@ -32,14 +48,23 @@ public class IndexEntry{
     citations.add(new SermCit(entry));
   }
 
+ /** Return phrase
+  * @return phrase
+  */
   public String getWord(){
     return word;
   }
 
+  /** Return list of citations
+   * @return list of citations as SermCit objects
+   */
   public List<SermCit> getCits(){
     return citations;
   }
 
+ /** Add a new reference to a preexisting IndexEntry object
+  * @param newCit the SermCit citation
+  */
   public void add(SermCit newCit){
     if (citations == null){
       citations = new ArrayList<SermCit>();
@@ -61,21 +86,37 @@ public class IndexEntry{
     }
   }
 
+ /** Add a series of citations as SermCit objects
+  * @param citList List of SermCit objects
+  */
   public void add(List<SermCit> citList){
     for (SermCit x : citList){
       add(x);
     }
   }
 
+  /** Add a citation in sermon, para form
+   * @param sermon Sermon containing reference
+   * @param para Paragraph within sermon
+   */
   public void add(String sermon, int para){
     SermCit cit = new SermCit(sermon, para);
     this.add(cit);
   }
 
+  /** Compare to another IndexEntry object
+   * @param other Other IndexEntry to compare to
+   * @return negative for this before other, 0 for equal, positive for this after other
+   */
+   @Override
   public int compareTo(IndexEntry other){
     return word.compareTo(other.getWord());
   }
 
+/** Turn into string
+ * @return IndexEntry in string form, e.g. "Virgin Mary   1.3, 2.7, 2.9"
+ */
+ @Override
   public String toString(){
     String result = word + "\t";
     for (SermCit cit : citations){
